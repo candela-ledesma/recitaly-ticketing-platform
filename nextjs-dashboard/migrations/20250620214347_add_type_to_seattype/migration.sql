207d5fefd7ae4775bdaaa234fb61d7dfd4d44a2f
candeLedesma <104953006+candeLedesma@users.noreply.github.com>
2026-05-04 23:41:44 -0300

    chore: remove prisma migrations (generated files)

--- nextjs-dashboard/migrations/20250620214347_add_type_to_seattype/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "SeatType" ALTER COLUMN "type" DROP DEFAULT;
--- nextjs-dashboard/migrations/20250902142709_add_recital_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "RecitalStatus" AS ENUM ('ACTIVE', 'INACTIVE');
-
--- AlterTable
-ALTER TABLE "Recital" ADD COLUMN     "status" "RecitalStatus" NOT NULL DEFAULT 'ACTIVE';
